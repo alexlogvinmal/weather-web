5eb8c27d409fcf8e1ec256bfd9dcf74c3f086f4d
alexlogvinmal <alexlogvinmal@gmail.com>
2023-03-19 03:03:17 +0200

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 import { setupStore } from './store';
+import { usePosition } from 'use-position';
+import { useEffect } from 'react';
+import { fetchWeather } from './services/fetchWeather';
+import { useAppDispatch } from '../src/redux/hook';
 
 function App() {
-
   const store = setupStore();
+
+  // const { latitude, longitude } = usePosition(false);
+  // const dispatch = useAppDispatch();
+
+  // useEffect(() => {
+  //   if (latitude && longitude) dispatch(fetchWeather({ lat: latitude, lon: longitude }))
+  // }, [latitude, longitude])
+
+
 
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-
+        <h1>Hello</h1>
       </Provider>
     </ThemeProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
 import {createRoot} from 'react-dom/client'
 import  App  from './App'
-import {usePosition} from 'use-position';
-import { useEffect } from 'react';
-import { fetchWeather } from './services/fetchWeather';
-import { useAppDispatch } from '../src/redux/hook';
+
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
-const {latitude, longitude} = usePosition(false);
-const dispatch = useAppDispatch();
-
-useEffect(() => {
-    if(latitude && longitude) dispatch(fetchWeather({lat: latitude, lon: longitude}))
-}, [latitude, longitude])
 
 root.render(<App />)
